9460e622659cb76580a5718e3353f5eab40d45ea
Jared Galanis <jaredgalanis@gmail.com>
2021-01-09 08:18:16 -0500

    wip featured collections and items

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -14,10 +14,100 @@ class PageController extends ControllerBase {
    * @return array
    *   A simple renderable array.
    */
-  public function PageOne() {
+  public function collection(\Drupal\node\Entity\Node $collection) {
+    $featured_items = \Drupal::entityTypeManager()
+      ->getListBuilder('node')
+      ->getStorage()
+      ->loadByProperties([
+        'field_featured_item' => true,
+        'type.target_id' => 'islandora_object',
+        'field_member_of' => $collection->id()
+      ]);
+
+    $featured_items_array = array();
+
+    foreach ($featured_items as &$item) {
+      $item_id = $item->id();
+
+      $media = \Drupal::entityTypeManager()
+        ->getListBuilder('media')
+        ->getStorage()
+        ->loadByProperties([
+          'bundle' => 'image',
+          'status' => 1,
+          'field_media_of' => $item_id,
+        ]);
+
+      $thumbnail_id = array_values($media)[0]->thumbnail->target_id;
+
+      $thumbnail = \Drupal::entityTypeManager()
+      ->getStorage('file')
+      ->load($thumbnail_id);
+
+      $image_url = $thumbnail->getFileUri();
+
+      $image_display_url = file_create_url($image_url);
+
+      $obj= (object) ['url' => $image_display_url, 'item' => $item];
+
+      array_push($featured_items_array, $obj);
+    }
+
     return [
-      '#theme' => 'my_block_template',
-      '#test_var' => $this->t('moo'),
+      '#theme' => 'page--collection',
+      '#collection' => $collection,
+      '#featured_items' => $featured_items_array,
+    ];
+  }
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function collections() {
+    $featured_collections = \Drupal::entityTypeManager()
+      ->getListBuilder('node')
+      ->getStorage()
+      ->loadByProperties([
+        'field_featured_item' => true,
+        'type.target_id' => 'collection_object'
+      ]);
+
+    $featured_collections_array = array();
+    // var_dump($featured_collections);
+
+    foreach ($featured_collections as &$collection) {
+      $collection_id = $collection->id();
+
+      $media = \Drupal::entityTypeManager()
+        ->getListBuilder('media')
+        ->getStorage()
+        ->loadByProperties([
+          'bundle' => 'image',
+          'status' => 1,
+          'field_media_of' => $collection_id,
+        ]);
+
+      $thumbnail_id = array_values($media)[0]->thumbnail->target_id;
+
+      $thumbnail = \Drupal::entityTypeManager()
+      ->getStorage('file')
+      ->load($thumbnail_id);
+
+      $image_url = $thumbnail->getFileUri();
+
+      $image_display_url = file_create_url($image_url);
+
+      $obj= (object) ['url' => $image_display_url, 'collection' => $collection];
+
+      array_push($featured_collections_array, $obj);
+    }
+
+    return [
+      '#theme' => 'page--collections',
+      '#featured_collections' => $featured_collections_array,
     ];
   }
 
