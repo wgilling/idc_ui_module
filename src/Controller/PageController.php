6a764a08a6f5377f7dd21afa4f08a060543d33e6
Jared Galanis <jaredgalanis@gmail.com>
2021-01-11 06:15:11 -0500

    add item route and links between routes

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -7,6 +7,57 @@ use Drupal\Core\Controller\ControllerBase;
  * Provides route responses for the Example module.
  */
 class PageController extends ControllerBase {
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function collections() {
+    $featured_collections = \Drupal::entityTypeManager()
+      ->getListBuilder('node')
+      ->getStorage()
+      ->loadByProperties([
+        'field_featured_item' => true,
+        'type.target_id' => 'collection_object'
+      ]);
+
+    $featured_collections_array = array();
+    // var_dump($featured_collections);
+
+    foreach ($featured_collections as &$collection) {
+      $collection_id = $collection->id();
+
+      $media = \Drupal::entityTypeManager()
+        ->getListBuilder('media')
+        ->getStorage()
+        ->loadByProperties([
+          'bundle' => 'image',
+          'status' => 1,
+          'field_media_of' => $collection_id,
+        ]);
+
+      $thumbnail_id = array_values($media)[0]->thumbnail->target_id;
+
+      $thumbnail = \Drupal::entityTypeManager()
+      ->getStorage('file')
+      ->load($thumbnail_id);
+
+      if ($thumbnail) {
+        $image_url = $thumbnail->getFileUri();
+        $image_display_url = file_create_url($image_url);
+      }
+
+      $obj= (object) ['url' => $image_display_url, 'collection' => $collection];
+
+      array_push($featured_collections_array, $obj);
+    }
+
+    return [
+      '#theme' => 'page--collections',
+      '#featured_collections' => $featured_collections_array,
+    ];
+  }
 
   /**
    * Returns a simple page.
@@ -67,50 +118,10 @@ class PageController extends ControllerBase {
    * @return array
    *   A simple renderable array.
    */
-  public function collections() {
-    $featured_collections = \Drupal::entityTypeManager()
-      ->getListBuilder('node')
-      ->getStorage()
-      ->loadByProperties([
-        'field_featured_item' => true,
-        'type.target_id' => 'collection_object'
-      ]);
-
-    $featured_collections_array = array();
-    // var_dump($featured_collections);
-
-    foreach ($featured_collections as &$collection) {
-      $collection_id = $collection->id();
-
-      $media = \Drupal::entityTypeManager()
-        ->getListBuilder('media')
-        ->getStorage()
-        ->loadByProperties([
-          'bundle' => 'image',
-          'status' => 1,
-          'field_media_of' => $collection_id,
-        ]);
-
-      $thumbnail_id = array_values($media)[0]->thumbnail->target_id;
-
-      $thumbnail = \Drupal::entityTypeManager()
-      ->getStorage('file')
-      ->load($thumbnail_id);
-
-      if ($thumbnail) {
-        $image_url = $thumbnail->getFileUri();
-        $image_display_url = file_create_url($image_url);
-      }
-
-      $obj= (object) ['url' => $image_display_url, 'collection' => $collection];
-
-      array_push($featured_collections_array, $obj);
-    }
-
+  public function item(\Drupal\node\Entity\Node $item) {
     return [
-      '#theme' => 'page--collections',
-      '#featured_collections' => $featured_collections_array,
+      '#theme' => 'page--item',
+      '#item' => $item,
     ];
   }
-
 }
