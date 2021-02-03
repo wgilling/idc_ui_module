f8c2f3b923587a13b21553131e9cb8e9d5264a7c
Jared Galanis <jaredgalanis@gmail.com>
2021-02-03 11:29:23 -0500

    disable node view override

--- src/Controller/NodeDisplayController.php
+++ src/Controller/NodeDisplayController.php
@@ -13,12 +13,12 @@ class NodeDisplayController extends NodeViewController {
   public function view(EntityInterface $node, $view_mode = 'full', $langcode = NULL) {
     $node_type = $node->get('type')->getString();
 
-    if ($node_type == 'collection_object') {
-      return CollectionsController::collection($node);
-    } elseif ($node_type == 'islandora_object') {
-      return CollectionsController::item($node);
-    } else {
+    // if ($node_type == 'collection_object') {
+    //   return CollectionsController::collection($node);
+    // } elseif ($node_type == 'islandora_object') {
+    //   return CollectionsController::item($node);
+    // } else {
       return parent::view($node, $view_mode, $langcode);
-    }
+    // }
   }
 }
